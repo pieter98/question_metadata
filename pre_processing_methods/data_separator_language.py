871180339017d758a4dce748a287039572e9e6e1
Pieter Pangat <pieter.pangat@ugent.be>
2022-04-28 10:56:46 +0200

    [pre-processing] added data separator based on language

---
+++ pre_processing_methods/data_separator_language.py
@@ -0,0 +1,83 @@
+import os
+import json
+import shutil
+
+# utility imports
+from tqdm import tqdm
+
+'''
+===================================================================================================================
+METHOD: (folders_in)
+yields all directories within a directory
+===================================================================================================================
+'''
+def folders_in(dir_path):
+    for fname in os.listdir(dir_path):
+        if os.path.isdir(os.path.join(dir_path,fname)):
+            yield os.path.join(dir_path,fname)
+
+'''
+===================================================================================================================
+METHOD: (separate_data)
+Will iterate over all content of a directory and 
+
+PARAMS:
+dir_path:   path to directory containing the questions you want to process
+dest_path:  destination directory where the language separated data will be stored
+===================================================================================================================
+'''
+def separate_data(dir_path, dest_path):
+    dirs = [dir_path]
+    while len(dirs) > 0:
+        next_dir_path = dirs[0]
+        separate_data_helper(next_dir_path, dest_path)
+        
+        # check if dir contains other dirs and add these to the dirs list
+        dirs.extend(list(folders_in(next_dir_path)))
+        dirs.remove(next_dir_path)
+
+'''
+===================================================================================================================
+METHOD: (separate_data_helper)
+helper method for language separation
+
+PARAMS:
+dir_path:   path to directory containing the questions you want to process
+dest_path:  destination directory where the language separated data will be stored
+===================================================================================================================
+'''
+def separate_data_helper(dir_path, dest_path):
+
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
+
+    if os.path.isdir(dir_path):
+        pbar = tqdm(os.listdir(dir_path))
+        for file_name in pbar:          
+            try:  
+                file_path = os.path.join(dir_path, file_name)
+                if not os.path.isdir(file_path):
+                    lang_name = "default"
+                    with open(file_path) as file:
+                        if file:
+                            json_file = json.load(file)
+                            lang_name = json_file["Metadata"]["Language"]            
+
+
+                    dest_folder_path = os.path.join(dest_path, lang_name)
+                    if not os.path.exists(dest_folder_path):
+                        os.makedirs(dest_folder_path)
+                    
+                    dest_file_path = os.path.join(dest_folder_path, file_name)
+                    shutil.copyfile(file_path, dest_file_path)
+                    pbar.set_description("")
+            except:
+                pbar.set_description(file_name + " was not processed")
+
+'''
+# Example usage
+src = os.path.join(os.getcwd(), "Questions")
+dest = os.path.join(os.getcwd(), "Language Seperated Questions")
+dirs = os.listdir(src)
+separate_data(src, dest)
+'''
