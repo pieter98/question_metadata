3117290f8b6a87ca1b761b7343f7c3a4e025b6cb
Pieter Pangat <pieter.pangat@ugent.be>
2022-04-28 11:23:24 +0200

    [pre-processing] minor update to data separator

--- pre_processing_methods/data_separator_language.py
+++ pre_processing_methods/data_separator_language.py
@@ -11,10 +11,13 @@ METHOD: (folders_in)
 yields all directories within a directory
 ===================================================================================================================
 '''
+
+
 def folders_in(dir_path):
     for fname in os.listdir(dir_path):
-        if os.path.isdir(os.path.join(dir_path,fname)):
-            yield os.path.join(dir_path,fname)
+        if os.path.isdir(os.path.join(dir_path, fname)):
+            yield os.path.join(dir_path, fname)
+
 
 '''
 ===================================================================================================================
@@ -24,17 +27,32 @@ Will iterate over all content of a directory and
 PARAMS:
 dir_path:   path to directory containing the questions you want to process
 dest_path:  destination directory where the language separated data will be stored
+debug:      indicate if debug logging is activated
 ===================================================================================================================
 '''
-def separate_data(dir_path, dest_path):
+
+
+def separate_data(dir_path, dest_path, debug=False):
     dirs = [dir_path]
     while len(dirs) > 0:
         next_dir_path = dirs[0]
         separate_data_helper(next_dir_path, dest_path)
-        
+
         # check if dir contains other dirs and add these to the dirs list
         dirs.extend(list(folders_in(next_dir_path)))
         dirs.remove(next_dir_path)
+    
+    # print some statistics
+    if debug:
+        total_count = 0
+        for lang_dir in os.listdir(dest_path):
+            lang_dir_path = os.path.join(dest_path, lang_dir)
+            print(lang_dir + ": " + str(len([name for name in os.listdir(
+                lang_dir_path) if os.path.isfile(os.path.join(lang_dir_path, name))])))
+            total_count += len([name for name in os.listdir(
+                lang_dir_path) if os.path.isfile(os.path.join(lang_dir_path, name))])
+        print("TOTAL COUNT: " + str(total_count))
+
 
 '''
 ===================================================================================================================
@@ -46,6 +64,8 @@ dir_path:   path to directory containing the questions you want to process
 dest_path:  destination directory where the language separated data will be stored
 ===================================================================================================================
 '''
+
+
 def separate_data_helper(dir_path, dest_path):
 
     if not os.path.exists(dest_path):
@@ -53,23 +73,22 @@ def separate_data_helper(dir_path, dest_path):
 
     if os.path.isdir(dir_path):
         pbar = tqdm(os.listdir(dir_path))
-        for file_name in pbar:          
-            try:  
+        for file_name in pbar:
+            try:
                 file_path = os.path.join(dir_path, file_name)
                 if not os.path.isdir(file_path):
-                    lang_name = "default"
+                    lang_name = "__default__"
                     with open(file_path) as file:
                         try:
                             json_file = json.load(file)
-                            lang_name = json_file["Metadata"]["Language"]        
+                            lang_name = json_file["Metadata"]["Language"]
                         except:
-                            lang_name = "default"
-
+                            lang_name = "__default__"
 
                     dest_folder_path = os.path.join(dest_path, lang_name)
                     if not os.path.exists(dest_folder_path):
                         os.makedirs(dest_folder_path)
-                    
+
                     dest_file_path = os.path.join(dest_folder_path, file_name)
                     shutil.copyfile(file_path, dest_file_path)
                     pbar.set_description("")
@@ -81,5 +100,5 @@ def separate_data_helper(dir_path, dest_path):
 src = os.path.join(os.getcwd(), "Questions")
 dest = os.path.join(os.getcwd(), "Language Seperated Questions")
 dirs = os.listdir(src)
-separate_data(src, dest)
+separate_data(src, dest, True)
 '''
